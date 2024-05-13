183021c4509d9449153c1d694bb69ab8301fbd04
bantheus <matheuschmidt.b@gmail.com>
2024-05-13 16:24:59 -0300

    🎉 initial commit

---
+++ src/test/java/com/bantheus/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bantheus.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
