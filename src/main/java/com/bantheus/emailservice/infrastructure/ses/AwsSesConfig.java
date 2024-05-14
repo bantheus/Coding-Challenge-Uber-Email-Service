8fb4f9b1687062ccb44fb7eb438a0cdbf1134766
bantheus <matheuschmidt.b@gmail.com>
2024-05-14 15:01:11 -0300

    ✨ feat: add aws ses config

---
+++ src/main/java/com/bantheus/emailservice/infrastructure/ses/AwsSesConfig.java
@@ -0,0 +1,15 @@
+package com.bantheus.emailservice.infrastructure.ses;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+
+@Configuration
+public class AwsSesConfig {
+  @Bean
+  public AmazonSimpleEmailService amazonSimpleEmailService() {
+    return AmazonSimpleEmailServiceClientBuilder.standard().build();
+  }
+}
